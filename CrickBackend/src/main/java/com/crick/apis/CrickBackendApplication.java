d51a11dda1c645238f913378183a786add666954
ankursalunke98 <ankursalunke1998@gmail.com>
2024-01-24 20:48:06 +0530

    first upload

---
+++ CrickBackend/src/main/java/com/crick/apis/CrickBackendApplication.java
@@ -0,0 +1,13 @@
+package com.crick.apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrickBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrickBackendApplication.class, args);
+	}
+
+}
